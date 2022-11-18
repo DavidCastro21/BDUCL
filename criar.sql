7fdc5c340d26376927b38cc31f7fbbb2d64ea48b
rubao112 <rubensilva2003@gmail.com>
2022-11-18 11:27:52 +0000

    11:27_18/11

--- criar.sql
+++ criar.sql
@@ -10,7 +10,7 @@ CREATE TABLE Equipa(
     nome                TEXT NOT NULL,
     pais                TEXT NOT NULL,
     montante_arrecadado INTEGER DEFAULT 17.74 CHECK(montante_arrecadado >= 17.74),
-    pontos              INTEGER DEFAULT 0,
+    pontos              INTEGER DEFAULT 0 CHECK(pontos >= 0),
     idEstadio           INTEGER REFERENCES Estadio (id)
 );
 
@@ -19,7 +19,7 @@ DROP TABLE IF EXISTS Jogador;
 CREATE TABLE Jogador(
     id                  INTEGER PRIMARY KEY NOT NULL,
     nome                TEXT NOT NULL UNIQUE,
-    numero              INTEGER NOT NULL,
+    numero              INTEGER NOT NULL CHECK(numero > 0),
     idEquipa            INTEGER REFERENCES Equipa (id) NOT NULL
 );
 
@@ -34,7 +34,7 @@ CREATE TABLE Fase(
 DROP TABLE IF EXISTS Jornada;
 CREATE TABLE Jornada(
     id                  INTEGER PRIMARY KEY NOT NULL,
-    numero              INTEGER NOT NULL,
+    numero              INTEGER NOT NULL CHECK (numero > 0),
     idFase              INTEGER REFERENCES Fase(id) NOT NULL
 );
 --Table Jogo
@@ -95,6 +95,6 @@ CREATE TABLE EventoOutro(
 DROP TABLE IF EXISTS Evento;
 CREATE TABLE Evento(
     id                  INTEGER PRIMARY KEY NOT NULL,
-    minuto              TEXT NOT NULL,
+    minuto              TEXT NOT NULL CHECK (minuto >= 0),
     idJogo              INTEGER REFERENCES Jogo (id)  NOT NULL
 );
